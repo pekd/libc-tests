78014c0dc706cc7594770fbb2266c34af321598d
Daniel Pekarek <pekarekdaniel@gmail.com>
2017-11-28 15:15:29 +0100

    Add option to specify bitcode file

--- run-musl.sh
+++ run-musl.sh
@@ -4,10 +4,15 @@ total=0
 ok=0
 failed=0
 
+BITCODE="musl-tests.bc"
+if [[ "$#" -eq 1 ]]; then
+	BITCODE="$1"
+fi
+
 for i in `seq 0 26`; do
 	total=$(($total + 1))
 	echo "Running test $i..."
-	llin musl-tests.bc $i
+	llin $BITCODE $i
 	if [ $? != 0 ]; then
 		echo "Test $i failed"
 		failed=$(($failed + 1))
